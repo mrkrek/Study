6128fa86fd25fd09b298b54bf6b2a6d092e002fc
mrkrek <111693150+mrkrek@users.noreply.github.com>
2022-09-20 15:07:28 +0300

    newspaper-второе задание

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -14,6 +14,9 @@ class Author(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=55, unique=True)
 
+    def __str__(self):
+        return self.name.title()
+
 
 class Post(models.Model):
     article = 'ar'
@@ -44,6 +47,8 @@ class Post(models.Model):
         size = 124 if len(self.text) > 124 else len(self.text)
         return self.text[:size] + '...'
 
+    def __str__(self):
+        return f'{self.news.title()}: {self.title[:20]}'
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,23 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = 'author'
+    template_name = 'posts.html'
+    context_object_name = 'id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news_2.html'
+    context_object_name = 'id_news_2'
